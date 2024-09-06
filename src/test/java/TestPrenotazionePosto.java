1e2ee75e8c02a34213b52c6ff7da36897b05e166
Marco <marco.pratesi@hotmail.it>
2024-09-06 15:55:29 +0200

    eccezioni finite

--- src/test/java/TestPrenotazionePosto.java
+++ src/test/java/TestPrenotazionePosto.java
@@ -9,7 +9,7 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TestPrenotazioneAula {
+public class TestPrenotazionePosto {
     @Test
     void prenotazioneTest() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
         startTheAppForPersistenceLayer();
